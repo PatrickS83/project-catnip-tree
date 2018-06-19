5339108a53d84b42d136a4a2f909e7bcc9f49680
Patrick Schuelke <p-schuelke@gmx.de>
2018-06-19 15:56:10 +0200

    add test route for posting a post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
+
+const Post = mongoose.model('posts');
 
 router.get('/', (req, res) => res.send('homeroute works'));
 router.get('/api/posts', (req, res) => res.send('subrouting works'));
+
+router.post('/api/posts', (req, res) => {
+  const { subject, content } = req.body;
+  const post = new Post({
+    subject,
+    content,
+    created: Date.now()
+  })
+    .save()
+    .then(post => res.send(post))
+    .catch(err => res.status(422).send(err));
+});
 
 module.exports = router;
