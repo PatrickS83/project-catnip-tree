aa9c461cf1a10b1a38956b75c4f215fdc5833bae
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-23 16:00:19 +0200

    add join date to User model

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   googleId: String,
   liked: { type: [String], default: [] },
-  disliked: { type: [String], default: [] }
+  disliked: { type: [String], default: [] },
+  joined: { type: Date, default: Date.now }
 });
 
 mongoose.model('users', userSchema);
