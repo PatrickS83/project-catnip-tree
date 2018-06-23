a269f3b2fa2b8a981942beba9bca05bc44261fd8
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-23 16:01:11 +0200

    add route descriptions and route for liking posts

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,20 +3,44 @@ const mongoose = require('mongoose');
 const router = express.Router();
 
 const Post = mongoose.model('posts');
+const User = mongoose.model('users');
 
-// /api/posts routes
-router.get('/', (req, res) => res.send('subrouting works'));
+// @route   GET api/posts/test
+// @desc    Tests post route
+// @access  Public
+router.get('/test', (req, res) => res.send('postroute works'));
 
+// @route   POST api/posts/
+// @desc    Creates a post
+// @access  Protected
 router.post('/', (req, res) => {
   const { subject, content } = req.body;
-  const post = new Post({
+  new Post({
     subject,
     content,
+    creator: req.user.id,
     created: Date.now()
   })
     .save()
     .then(post => res.send(post))
     .catch(err => res.status(422).send(err));
+});
+
+// @route   POST api/posts/like/:id
+// @desc    Like a post
+// @access  Protected
+router.post('/like/:id', async (req, res) => {
+  const user = await User.findById('5b2e3f6e5d109200dc65efe6');
+  const post = await Post.findById(req.params.id);
+  const alreadyLiked = user.liked.find(postID => postID === post.id);
+  if (alreadyLiked) return res.status(400).send('User already liked this post');
+  post.likes += 1;
+  user.liked.unshift(post.id);
+  // post.save();
+  // user.save();
+  Promise.all([post.save(), user.save()])
+    .then(result => res.json({ result }))
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
