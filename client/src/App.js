02bc529600de2a565ba7d94ec0bb3c19ea44868e
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-25 17:53:36 +0200

    installed client with create-react-app and added eslint config

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <div className="App">React App is working correctly!</div>;
+  }
+}
+
+export default App;
