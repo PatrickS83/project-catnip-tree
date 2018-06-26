74d358db536af79f1be8f727432f4cddfc5c4b58
Patrick Schuelke <p-schuelke@gmx.de>
2018-06-26 15:40:50 +0200

    refactored getPosts using async/await

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,19 +1,11 @@
 import axios from 'axios';
 import { GET_POSTS } from './types';
 
-export const getPosts = () => dispatch => {
-  axios
-    .get('/api/posts')
-    .then(res =>
-      dispatch({
-        type: GET_POSTS,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_POSTS,
-        payload: null
-      })
-    );
+export const getPosts = () => async dispatch => {
+  try {
+    const res = axios.get('/api/posts');
+    dispatch({ type: GET_POSTS, payload: res.data });
+  } catch (err) {
+    dispatch({ type: GET_POSTS, payload: null });
+  }
 };
