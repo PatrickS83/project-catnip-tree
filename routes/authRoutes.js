7ea3c56a21d8e0ba1c3f8b3d5e4207b7d2e62995
Patrick Schuelke <p-schuelke@gmx.de>
2018-06-27 14:38:41 +0200

    add redirect to home on logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -29,7 +29,7 @@ router.get('/current_user', (req, res) => {
 // @desc    Logs user out
 router.get('/logout', (req, res) => {
   req.logout();
-  res.send('Successfully logged out!');
+  res.redirect('/');
 });
 
 module.exports = router;
