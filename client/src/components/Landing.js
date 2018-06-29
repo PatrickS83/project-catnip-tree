d749db9eba03dea742cd25b833b32527d0b0e33f
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-29 16:26:33 +0200

    LandingPage now stores all posts from db in redux state

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
+import { Button, Container } from 'semantic-ui-react';
+import * as postActions from '../actions/postActions';
 
 class Landing extends Component {
+  static propTypes = {
+    getPosts: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    const { getPosts } = this.props;
+    getPosts();
+  }
+
   render() {
     return (
-      <div>
+      <Container>
         <h1>Welcome to the Landing Page</h1>
         <Link to="/posts/createPost">
           <Button content="Create a post!" />
         </Link>
-      </div>
+      </Container>
     );
   }
 }
 
-export default Landing;
+export default connect(
+  null,
+  postActions
+)(Landing);
