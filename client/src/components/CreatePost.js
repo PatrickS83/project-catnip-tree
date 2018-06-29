c86245e22dc91d30650d367e38327b1d38bf3b6b
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-29 12:20:44 +0200

    add boilerplate code to link to "create a post" component

---
+++ client/src/components/CreatePost.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CreatePost = () => (
+    <div>
+      <h1>Create a new Post !!!</h1>
+    </div>
+  );
+
+export default CreatePost;
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
   render() {
     return (
       <div>
         <h1>Welcome to the Landing Page</h1>
+        <Link to="/posts/createPost">
+          <button>Create a post!</button>
+        </Link>
       </div>
     );
   }
