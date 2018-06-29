397985f82dd5ad89ccb19def5a8431b98617dd72
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-29 12:06:59 +0200

    created PrivateRoute Component for protected routes

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const PrivateRoute = ({ component: Component, auth, ...rest }) => {
+  if (auth === null) return null;
+  return (
+    <Route
+      {...rest}
+      render={props => (auth ? <Component {...props} /> : <Redirect to={{ pathname: '/' }} />)}
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.func.isRequired,
+  auth: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.shape({
+      liked: PropTypes.array.isRequired,
+      disliked: PropTypes.array.isRequired,
+      _id: PropTypes.string.isRequired,
+      googleId: PropTypes.string.isRequired,
+      joined: PropTypes.string.isRequired
+    })
+  ])
+};
+
+PrivateRoute.defaultProps = { auth: null };
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
