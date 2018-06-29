b519777ec51c32de55e4909aebf59fd613949d60
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-29 11:38:52 +0200

    commented out footer component for now

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
-import Footer from './Footer';
+// import Footer from './Footer';
 
 const Layout = ({ children }) => (
   <div>
