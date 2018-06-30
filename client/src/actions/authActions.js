ec0178b65089ccd8b69908dd788ca662e1cb4a88
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-30 12:36:28 +0200

    add actions for liking and disliking a post

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, LIKE_POST, DISLIKE_POST } from './types';
 
 export const fetchUser = () => async dispatch => {
   try {
@@ -7,5 +7,23 @@ export const fetchUser = () => async dispatch => {
     dispatch({ type: FETCH_USER, payload: res.data });
   } catch (err) {
     dispatch({ type: FETCH_USER, payload: null });
+  }
+};
+
+export const likePost = postId => async dispatch => {
+  try {
+    const res = await axios.post(`/api/posts/like/${postId}`);
+    dispatch({ type: LIKE_POST, payload: res.data });
+  } catch (err) {
+    dispatch({ type: LIKE_POST, payload: null });
+  }
+};
+
+export const dislikePost = postId => async dispatch => {
+  try {
+    const res = await axios.post(`/api/posts/dislike/${postId}`);
+    dispatch({ type: DISLIKE_POST, payload: res.data });
+  } catch (err) {
+    dispatch({ type: DISLIKE_POST, payload: null });
   }
 };
