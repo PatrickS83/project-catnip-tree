37059307b1982f873b1832a3db4fa9c4fb9c2fea
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-30 12:50:16 +0200

    created redux types and reducers for liking/disliking posts

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,5 @@
 export const GET_POSTS = 'GET_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_USER = 'FETCH_USER';
+export const LIKE_POST = 'LIKE_POST';
+export const DISLIKE_POST = 'DISLIKE_POST';
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,9 +1,21 @@
-import { FETCH_USER } from '../actions/types';
+import { FETCH_USER, LIKE_POST, DISLIKE_POST } from '../actions/types';
 
 export default function(state = null, action) {
   switch (action.type) {
     case FETCH_USER:
       return action.payload || false;
+    case LIKE_POST:
+      return {
+        ...state,
+        liked: action.payload[1].liked,
+        disliked: action.payload[1].disliked
+      };
+    case DISLIKE_POST:
+      return {
+        ...state,
+        liked: action.payload[1].liked,
+        disliked: action.payload[1].disliked
+      };
     default:
       return state;
   }
