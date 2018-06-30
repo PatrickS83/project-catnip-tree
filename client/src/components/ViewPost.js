0126b46f380932d9b1ec0011605f7200519f2cef
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-30 14:58:20 +0200

    created baseline component to view a single post

---
+++ client/src/components/ViewPost.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Container, Item, Loader } from 'semantic-ui-react';
+import PostItem from './PostItem';
+import * as postActions from '../actions/postActions';
+
+class ViewPost extends Component {
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    post: PropTypes.shape({
+      _id: PropTypes.string,
+      creator: PropTypes.shape({ nick: PropTypes.string }),
+      subject: PropTypes.string,
+      content: PropTypes.string,
+      likes: PropTypes.number,
+      dislikes: PropTypes.number
+    }).isRequired,
+    viewPost: PropTypes.func.isRequired,
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        id: PropTypes.string.isRequired
+      })
+    }).isRequired
+  };
+
+  componentDidMount() {
+    const { viewPost, match } = this.props;
+    viewPost(match.params.id);
+  }
+
+  render() {
+    const {
+      loading,
+      post: { _id, creator, subject, content, likes, dislikes }
+    } = this.props;
+    if (!_id) return null;
+    return (
+      <Container>
+        <Item.Group>
+          {!loading ? (
+            <PostItem
+              subject={subject}
+              content={content}
+              likes={likes}
+              dislikes={dislikes}
+              author={creator.nick}
+              id={_id}
+            />
+          ) : (
+            <Loader active inline="centered" size="massive">
+              Loading Post ...
+            </Loader>
+          )}
+        </Item.Group>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = ({ post }) => ({ post: post.post, ...post });
+
+export default connect(
+  mapStateToProps,
+  postActions
+)(ViewPost);
