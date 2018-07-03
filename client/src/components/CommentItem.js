51c7db595c4659d6b49c9e29dbee52fcf76b0d93
Patrick Schuelke <p-schuelke@gmx.de>
2018-07-03 14:06:03 +0200

    Comments are now correctly displayed and can be created users

--- client/src/components/CommentItem.js
+++ client/src/components/CommentItem.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Comment } from 'semantic-ui-react';
+import moment from 'moment';
 import avatar from './img/image.png';
 
-const CommentItem = () => (
+const CommentItem = ({ user, created, text, nick }) => (
   <Comment>
     <Comment.Avatar src={avatar} />
     <Comment.Content>
-      <Comment.Author as="a">Matt</Comment.Author>
+      {/* preventdefault because profiles are not implemented yet
+      TODO: implement profiles */}
+      <Comment.Author as="a" href={`/profile/${user}`} onClick={e => e.preventDefault()}>
+        {nick}
+      </Comment.Author>
       <Comment.Metadata>
-        <div>Today at 5:42PM</div>
+        <div>{moment(created).fromNow()}</div>
       </Comment.Metadata>
-      <Comment.Text>Great Comment</Comment.Text>
+      <Comment.Text>{text}</Comment.Text>
       <Comment.Actions>
         <Comment.Action>Reply</Comment.Action>
       </Comment.Actions>
     </Comment.Content>
   </Comment>
 );
+
+CommentItem.propTypes = {
+  user: PropTypes.string.isRequired,
+  created: PropTypes.string.isRequired,
+  nick: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
+};
 
 export default CommentItem;
--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,20 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Button, Comment, Form, Header } from 'semantic-ui-react';
 import CommentItem from './CommentItem';
+import * as postActions from '../actions/postActions';
 
-const CommentExampleComment = () => (
-  <Comment.Group>
-    <Header as="h3" dividing>
-      Comments
-    </Header>
+class Comments extends Component {
+  static propTypes = {
+    createComment: PropTypes.func.isRequired,
+    post: PropTypes.shape({
+      _id: PropTypes.string,
+      comments: PropTypes.arrayOf(PropTypes.object)
+    }).isRequired,
+    auth: PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      nick: PropTypes.string.isRequired
+    }).isRequired
+  };
 
-    <CommentItem />
+  state = { comment: '' };
 
-    <Form reply>
-      <Form.TextArea />
-      <Button content="Add Reply" labelPosition="left" icon="edit" color="orange" />
-    </Form>
-  </Comment.Group>
-);
+  handleChange = ({ currentTarget }) =>
+    this.setState({ [currentTarget.name]: currentTarget.value });
 
-export default CommentExampleComment;
+  render() {
+    const { comment } = this.state;
+    const {
+      createComment,
+      post: { _id: postID, comments },
+      auth: { _id: userID, nick }
+    } = this.props;
+
+    return (
+      <Comment.Group>
+        <Header as="h3" dividing>
+          Comments
+        </Header>
+        {comments.length
+          ? comments.map(item => (
+              <CommentItem
+                key={item._id}
+                user={item.user}
+                created={item.created}
+                nick={nick}
+                text={item.text}
+              />
+            ))
+          : null}
+
+        <Form reply onSubmit={() => createComment(postID, { userID, comment })}>
+          <Form.TextArea onChange={this.handleChange} name="comment" value={comment} />
+          <Button content="Add Reply" labelPosition="left" icon="edit" color="orange" />
+        </Form>
+      </Comment.Group>
+    );
+  }
+}
+
+const mapStateToProps = ({ post, auth }) => ({ post: post.post, auth });
+
+export default connect(
+  mapStateToProps,
+  postActions
+)(Comments);
